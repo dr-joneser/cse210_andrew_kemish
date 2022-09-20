a3cd1f33ddbf6db334923da280b2c132c5923089
dr-joneser <andrew.joneser1@gmail.com>
2022-09-20 15:46:24 -0600

    Merge branch 'main' of https://github.com/dr-joneser/cse210_andrew_kemish

--- Program.cs
+++ Program.cs
@@ -1,32 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Get input from user and convert to int
 
 Console.Write("What is your grade percent? ");
 string? numberInText = Console.ReadLine();
-int number = int.Parse(numberInText);
+int number;
+number = Convert.ToInt32(numberInText);
 
+// Determin what letter grade is appropriate and display to terminal
 if (number >= 90)
 {
     string letterGrade = "A";
+    Console.Write($"Your letter grade is: {letterGrade}");
 }
 else if (number >= 80)
 {
     string letterGrade = "B";
+    Console.Write($"Your letter grade is: {letterGrade}");
 }
 else if (number >= 70)
 {
     string letterGrade = "C";
+    Console.Write($"Your letter grade is: {letterGrade}");
 }
 else if (number >= 60)
 {
     string letterGrade = "D";
+    Console.Write($"Your letter grade is: {letterGrade}");
 }
 else
 {
     string letterGrade = "F";
+    Console.Write($"Your letter grade is: {letterGrade}");
 }
 
-Console.Write($"Your letter grade is: {letterGrade}");
+// Print on a new line.
+Console.WriteLine(" ");
 
+// Display message if above 70%
 if (number >= 70)
 {
     Console.Write("Congratulations! You passed the class!");
