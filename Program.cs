0b9aba87e864e41ca3671b7ca45aa01e3bee8594
dr-joneser <andrew.joneser1@gmail.com>
2022-09-20 15:27:41 -0600

    Solo prep 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("What is your first name? ");
-string? firstName = Console.ReadLine();
-Console.Write("What is your last name? ");
-string? lastName = Console.ReadLine();
-Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+Console.Write("What is your grade percent? ");
+string? numberInText = Console.ReadLine();
+int number = int.Parse(numberInText);
+
+if (number >= 90)
+{
+    string letterGrade = "A";
+}
+else if (number >= 80)
+{
+    string letterGrade = "B";
+}
+else if (number >= 70)
+{
+    string letterGrade = "C";
+}
+else if (number >= 60)
+{
+    string letterGrade = "D";
+}
+else
+{
+    string letterGrade = "F";
+}
+
+Console.Write($"Your letter grade is: {letterGrade}");
+
+if (number >= 70)
+{
+    Console.Write("Congratulations! You passed the class!");
+}
+else
+{
+    Console.Write("Stay focused and you'll get it next time!");
+}
