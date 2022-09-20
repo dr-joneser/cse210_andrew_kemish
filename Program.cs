7e2f089d3e9ff161081c73d353c22f871fa970f5
dr-joneser <andrew.joneser1@gmail.com>
2022-09-20 17:49:01 -0600

    Solo Prep #4
    Andrew Jones

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
-﻿Random randomGenerator = new Random();
-int number = randomGenerator.Next(1, 100);
-
-int guess = 0;
-
-while (guess != number)
+﻿using System.Collections.Generic;
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+List<int> numbers = new List<int>();
+int value = -1;
+while (value != 0)
 {
-    Console.WriteLine(" ");
-    Console.Write("What is your guess? ");
-    guess = int.Parse(Console.ReadLine());
-
-    if (guess < number)
+    Console.WriteLine("Enter Number: ");
+    value = int.Parse(Console.ReadLine());
+    if (value != 0)
     {
-        Console.Write("Higher");
-    }
-    else if (guess > number)
-    {
-        Console.Write("Lower");
-    }
-    else
-    {
-        Console.Write("You guessed it!");
+        numbers.Add(value);
     }
 }
+int sum = 0;
+foreach (int num in numbers)
+{
+    sum += num;
+}
+Console.WriteLine($"The sum is {sum}");
+int count = numbers.Count;
+int average = (sum / count);
+Console.WriteLine($"The average is {average}");
+int best = 0;
+foreach (int num in numbers)
+{
+    if (num > best)
+    {
+        best = num;
+    }
+}
+Console.WriteLine($"The largest number is {best}");
