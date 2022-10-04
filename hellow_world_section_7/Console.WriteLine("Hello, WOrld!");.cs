813d8c6060d82db2cf747717e56bcada50cd0ebf
kemishg <kemish.gip@gmail.com>
2022-10-04 12:59:27 -0600

    Delete Console.WriteLine("Hello, WOrld!");.cs

--- hellow_world_section_7/Console.WriteLine("Hello, WOrld!");.cs
+++
@@ -1,8 +0,0 @@
-
-first = input("What is your first name? ");
-last = input("What is your last name? ");
-
-
-string first = Console.ReadLine();
-string last = Console.Readline();
-Console.WriteLine($"Your name is {last}, {first} {last}.");
