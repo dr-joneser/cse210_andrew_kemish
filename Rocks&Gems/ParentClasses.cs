e1003cc5833cbfc29a72f0f43988d688843bb08c
dr-joneser <andrew.joneser1@gmail.com>
2022-11-01 12:50:54 -0600

    abstraction

--- Rocks&Gems/ParentClasses.cs
+++ Rocks&Gems/ParentClasses.cs
@@ -1,13 +1,13 @@
-class Position {
+abstract class Position {
     // CONTAINS THE X AND Y COORDINATES FOR ALL OBJECTS THAT WILL BE ON SCREEN
     public int xPosition = new int();
     public int yPosition = new int();
 }
 
-class MovementHorizontal: Position {
+abstract class MovementHorizontal: Position {
     // X-VALUE POSITION OF PLAYER IS CHANGED WHEN LEFT OR RIGHT ARROW KEYS ARE PRESSED
 }
 
-class MovementVertical: Position {
+abstract class MovementVertical: Position {
     // Y-VALUE POSITION OF ROCKS AND GEMS ARE INCREASING AT A CONSTANT RATE (GRAVITY)
 }
