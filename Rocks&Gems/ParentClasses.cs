6f1c8b18198af6ae8f5c6ed3e32c614b49ff749e
dr-joneser <andrew.joneser1@gmail.com>
2022-11-01 13:11:34 -0600

    adding raylib

--- Rocks&Gems/ParentClasses.cs
+++ Rocks&Gems/ParentClasses.cs
@@ -1,11 +1,20 @@
 abstract class Position {
     // CONTAINS THE X AND Y COORDINATES FOR ALL OBJECTS THAT WILL BE ON SCREEN
-    public int xPosition = new int();
-    public int yPosition = new int();
+    public int xPos = new int();
+    public int yPos = new int();
 }
 
 abstract class MovementHorizontal: Position {
     // X-VALUE POSITION OF PLAYER IS CHANGED WHEN LEFT OR RIGHT ARROW KEYS ARE PRESSED
+    //
+    //if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+    //     PlayerRectangle.x += MovementSpeed;
+    //}
+    //
+    //if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+    //     PlayerRectangle.x -= MovementSpeed;
+    //}
+
 }
 
 abstract class MovementVertical: Position {
