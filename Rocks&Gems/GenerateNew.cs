7c0acdf01c0db3377c875130c3baa465fd74b13e
dr-joneser <andrew.joneser1@gmail.com>
2022-11-01 18:14:42 -0600

    trying to get raylib to work

---
+++ Rocks&Gems/GenerateNew.cs
@@ -0,0 +1,6 @@
+class GenerateNewObj {
+    
+    //Randomly generate new rock and gem objects and add to list that contains all the objects
+    //Need to be in a single list so that we can run through a for loop and check for collisions
+    
+}
--- Rocks&Gems/Score.cs
+++ Rocks&Gems/Score.cs
@@ -1,4 +1,15 @@
+
+
 class PlayerScore: Position{
     public int score = new int();
+
+    //SCORE POSITION
+    xPos = 12;
+    yPos = 12;
+
+    // DISPLAY SCORE
+    public static void DrawScore(int score) {
+        Raylib.DrawText("text", xPos, yPos, 10, GRAY);
+    }
 
 }
