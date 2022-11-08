520f61cdd7fac110393e6d2d063cb66374e10f31
kemishg <kemish.gip@gmail.com>
2022-11-08 13:11:36 -0700

    Revert "gem"
    This reverts commit 04418e0a7372c244cfd5723bd9d9853a2bbeb933.

--- Rocks&Gems/Main.cs
+++ Rocks&Gems/Main.cs
@@ -21,11 +21,9 @@ class MainRocks {
             Raylib.InitWindow(ScreenWidth, ScreenHeight, "Rocks and Gems");
             Raylib.SetTargetFPS(60);
             int score = 0;
-            var PlayerPosition = new Vector2 (400,450);
 
             while (!Raylib.WindowShouldClose())
             {
-<<<<<<< HEAD
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
 
@@ -65,7 +63,6 @@ class MainRocks {
                 }
 
                 Raylib.EndDrawing();
-=======
                 // INTRO TEXT 
                 // Raylib.DrawText("Move the red square to the blue square with the arrow keys!", 12, 12, 20, Color.BLACK);
                 Raylib.DrawText($"Score: {score}",12,12,15,Color.BLACK);
@@ -79,15 +76,16 @@ class MainRocks {
                 switch (RockOrGem){
                     // If case is 0 it'll create a Rock
                     case 0:
+                    Console.WriteLine("Creating a Rock");
                         var Rock = new GameRock();
                         Rock.Position = position;
-                        Rock.Velocity = new Vector2(0,14);
+                        Rock.Velocity = new Vector2(0,18);
                         Rocks.Add(Rock);
                         break;
                     case 1:
                         var Gem = new GameGem();
                         Gem.Position = position;
-                        Gem.Velocity = new Vector2(0,14);
+                        Gem.Velocity = new Vector2(0,-2);
                         Gems.Add(Gem);
                         break;
                     default:
@@ -97,8 +95,6 @@ class MainRocks {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
 
-
-
                 // Draw all of the objects in their current location
                 foreach (var obj in Rocks) {
                     obj.Draw();
@@ -107,18 +103,6 @@ class MainRocks {
                     obj.Draw();
                 }
 
-                var Player= new Player();
-                Player.Position = PlayerPosition;
-                Player.Velocity = new Vector2(2, 0); 
-                Player playerObj = new Player();
-
-                Player.Draw();
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
-                    PlayerPosition.X +=8;
-                }
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
-                    PlayerPosition.X -=8;
-                }
                 Raylib.EndDrawing();
 
                 // Move all of the objects to their next location
@@ -128,30 +112,11 @@ class MainRocks {
                 foreach (var obj in Gems) {
                     obj.MoveVertical();
                 }
-                var TheRectangle = new Rectangle(PlayerPosition.X,PlayerPosition.Y, 20, 20);
-
-                foreach (var obj in Rocks) {
-                    if (Raylib.CheckCollisionPointRec(obj.Position, TheRectangle)) {
-                        score -=1;
-                        Rocks.Add(obj);
-                        Rocks.Remove(obj);
-                }
-                }
-
-                foreach (var obj in Gems) {
-                    if (Raylib.CheckCollisionPointRec(obj.Position, TheRectangle)) {
-                        score +=1;
-                        Gems.Add(obj);
-                        Gems.Remove(obj); 
-                }
-                }
-
             }
 
             Raylib.CloseWindow();
         }
     }
-}
 
 
                 // if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
@@ -172,3 +137,8 @@ class MainRocks {
 
                 // Raylib.DrawRectangleRec(TargetRectangle, Color.BLUE);
                 // Raylib.DrawRectangleRec(PlayerRectangle, Color.RED);
+
+                // if (Raylib.CheckCollisionRecs(PlayerRectangle, TargetRectangle)) {
+                //     Raylib.DrawText("You did it!!!!", 12, 34, 20, Color.BLACK);
+                // }
+}
